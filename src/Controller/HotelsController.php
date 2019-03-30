3b9fe4070153072b9ce4148be018efd7f0608a1f
53045467 <48685916+53045467@users.noreply.github.com>
2019-03-30 21:31:57 +0100

    hotel et agence

---
+++ src/Controller/HotelsController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pc
+ * Date: 29/03/2019
+ * Time: 10:15
+ */
+
+namespace App\Controller;
+
+
+class HotelsController
+{
+
+}
---
+++ src/Controller/OffresController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pc
+ * Date: 30/03/2019
+ * Time: 20:53
+ */
+
+namespace App\Controller;
+
+
+class OffresController
+{
+
+}
