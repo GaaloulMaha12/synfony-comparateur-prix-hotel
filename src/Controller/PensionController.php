50b524a8d69ed7509d004eed16c40d184fcc3c86
marwalouati123 <48686384+marwalouati123@users.noreply.github.com>
2019-04-03 17:03:55 +0200

    pension

--- src/Controller/PensionController.php
+++ src/Controller/PensionController.php
@@ -9,7 +9,40 @@
 namespace App\Controller;
 
 
-class PensionController
+use App\Entity\Pension;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+class PensionController extends AbstractController
 {
+    /**
+     * @Route("/pension",name="pension")
+     */
+    public function pensionList (Request $request)
+    {
+
+        $pension = new Pension ();
+
+        $form = $this->createFormBuilder($pension)
+            ->add('typepension', TextType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        $repository = $this->getDoctrine()->getRepository(pension::class);
+        $pensionData = $repository->findAll();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $pension = $form->getData();
+            $entityManager->persist($pension);
+            $entityManager->flush();
+            $pension = $repository->findAll();
+
+            return $this->redirectToRoute('pension');
+
+
+        }
+    }
 
 }
