cf3dd4630f09bd7258dd86389075dab77dddef60
53045467 <48685916+53045467@users.noreply.github.com>
2019-04-11 15:26:55 +0200

    client

--- src/Controller/OffresController.php
+++ src/Controller/OffresController.php
@@ -7,8 +7,13 @@
  */
 
 namespace App\Controller;
+
+use App\Entity\Categoriechambre;
+use App\Entity\Pension;
 use App\Entity\Agence;
 use App\Entity\Hotel;
+use App\Entity\Chambre;
+use function Symfony\Bridge\Twig\Extension\twig_is_selected_choice;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use App\Entity\Offre;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -32,17 +37,36 @@ class OffresController extends AbstractController
         $repository = $this->getDoctrine()->getRepository(Agence::class);
         $agences = $repository->findAll();
         $agencesArray = array();
-        foreach($agences as $a => $val) {
-            $agencesArray[$val->getNomAgence()]= $val->getId();
+        foreach ($agences as $a => $val) {
+            $agencesArray[$val->getNomAgence()] = $val;
         }
-        $repository = $this->getDoctrine()->getRepository(Hotel::class);
-        $hotels = $repository->findAll();
+        $repository1 = $this->getDoctrine()->getRepository(Hotel::class);
+        $hotels = $repository1->findAll();
         $hotelsArray = array();
-        foreach($hotels as $a => $val) {
-            $hotelsArray[$val->getNomhotel()]= $val->getId();
+        foreach ($hotels as $a => $val) {
+            $hotelsArray[$val->getNomhotel()] = $val;
+        }
+        $repository2 = $this->getDoctrine()->getRepository(Chambre::class);
+        $chambres = $repository2->findAll();
+        $chambresArray = array();
+        foreach ($chambres as $a => $val) {
+            $chambresArray[$val->getTypechambre() ] = $val;
+        }
+        $repository3 = $this->getDoctrine()->getRepository(Categoriechambre::class);
+        $categoriechambre = $repository3->findAll();
+        $categoriechambreArray = array();
+        foreach ($categoriechambre as $a => $val) {
+            $categoriechambreArray[$val->getCategorie()] = $val;
+        }
+        $repository4 = $this->getDoctrine()->getRepository(Pension::class);
+        $pension = $repository4->findAll();
+        $pensionArray = array();
+        foreach ($pension as $a => $val) {
+            $pensionArray[$val->getTypepension()] = $val;
         }
 
-     //   var_dump($agences);
+
+        //   var_dump($agences);
 
         // jib liste des agences
         // declaration mta3 array vide
@@ -54,21 +78,27 @@ class OffresController extends AbstractController
             ->add('datedebut', TextType::class)
             ->add('datefin', TextType::class)
             ->add('agence', ChoiceType::class, [
-             'choices' => $agencesArray
-                    ]
+                    'choices' => $agencesArray
+                ]
             )
             ->add('hotel', ChoiceType::class, [
-                'choices' => $hotelsArray
-            ]
+                    'choices' => $hotelsArray
+                ]
             )
-            ->add('pension', ChoiceType::class)
-<<<<<<< HEAD
-            ->add('chambre', ChoiceType::class)
-            ->add('tarif', TextType::class)
-=======
-            ->add('chambres', ChoiceType::class)
-
->>>>>>> 63a1e366d4d242d3bfab55d2cb81b2f4e2b9ee9e
+            ->add('chambre', ChoiceType::class,
+                [
+                    'choices' => $chambresArray
+                ]
+            )
+            ->add('categoriechambre', ChoiceType::class,
+                [
+                    'choices' => $categoriechambreArray
+                ])
+            ->add('pension', ChoiceType::class,
+                [
+                    'choices' => $pensionArray
+                ])
+            ->add('tariflocal', TextType::class)
             ->add('save', SubmitType::class, ['label' => 'ajouter'])
             ->getForm();
 
@@ -78,45 +108,111 @@ class OffresController extends AbstractController
         $DealsData = $repository->findAll();
         if ($form->isSubmitted() && $form->isValid()) {
             $offre = $form->getData();
-            var_dump($offre);
-//            $entityManager = $this->getDoctrine()->getManager();
-//            $entityManager->persist($offre);
-//            $entityManager->flush();
+//            var_dump($offre);
+            $entityManager = $this->getDoctrine()->getManager();
+//
+//            $tarif = new Tarif();
+//            $tarif->setPrix($offre->getTarifLocal());
+//
+//            $entityManager->persist($tarif);
+//            $offre->setTarif($tarif);
+
+            $entityManager->persist($offre);
+            $entityManager->flush();
             $offre = $repository->findAll();
             return $this->redirectToRoute('offre');
 
         }
         return $this->render('admin/offres/offresList.html.twig', [
-            'offres' =>  $DealsData,
+            'offres' => $DealsData,
             'form' => $form->createView(),
 
         ]);
 
 
     }
+
     /**
      * @Route("/offres/edit/{id}" ,name="editoffre")
      */
-    public function editAgency(Request $request, $id)
+    public function editOffre(Request $request, $id)
     {
+
+
         $entityManager = $this->getDoctrine()->getManager();
-        $offre = $entityManager->getRepository(offre::class)->find($id);
+        $offre = $entityManager->getRepository(Offre::class)->find($id);
+        /* $agencesArray = array();
+         foreach ($agencesArray as $a => $val) {
+             $agencesArray[$val->setNomAgence()] = $val;
+         }
+ */
+        $repository = $this->getDoctrine()->getRepository(Agence::class);
 
+        $agences = $repository->findAll();
+        $agencesArray = array();
+        foreach ($agences as $a => $val) {
+            $agencesArray[$val->getNomAgence()] = $val;
+        }
+        $repository1 = $this->getDoctrine()->getRepository(Hotel::class);
 
+        $hotels = $repository1->findAll();
+        $hotelsArray = array();
+        foreach ($hotels as $a => $val) {
+            $hotelsArray[$val->getNomhotel()] = $val;
+        }
+        $repository2 = $this->getDoctrine()->getRepository(Chambre::class);
+
+        $chambres = $repository2->findAll();
+        $chambresArray = array();
+        foreach ($chambres as $a => $val) {
+            $chambresArray[$val->getTypechambre()] = $val;
+        }
+        $repository3= $this->getDoctrine()->getRepository(Categoriechambre::class);
+
+        $categories = $repository3->findAll();
+        $categoriesArray = array();
+        foreach ($categories as $a => $val) {
+            $categoriesArray[$val->getCategorie()] = $val;
+        }
+
+        $repository4 = $this->getDoctrine()->getRepository(Pension::class);
+
+        $pension = $repository4->findAll();
+        $pensionArray = array();
+        foreach ($pension as $a => $val) {
+            $pensionArray[$val->getTypepension()] = $val;
+        }
+//        var_dump($offre->getAgence()->getNomAgence());
         $form = $this->createFormBuilder($offre)
             ->add('nomoffre', TextType::class)
             ->add('datedebut', TextType::class)
             ->add('datefin', TextType::class)
-            ->add('agence', ChoiceType::class)
-            ->add('hotel', ChoiceType::class)
-            ->add('pension', ChoiceType::class)
-<<<<<<< HEAD
-            ->add('chambre', ChoiceType::class)
-            ->add('tarif', TextType::class)
-=======
-            ->add('chambres', ChoiceType::class)
-
->>>>>>> 63a1e366d4d242d3bfab55d2cb81b2f4e2b9ee9e
+            ->add('agence', ChoiceType::class,
+                [
+                    'choices' => $agencesArray,
+                    'empty_data' => $offre->getAgence()->getNomAgence()
+                ])
+            ->add('hotel', ChoiceType::class,
+                [
+                    'choices' => $hotelsArray,
+//                    'empty_data' => $offre->getHotel()->getNomhotel()
+                ])
+            ->add('chambre', ChoiceType::class,
+                [
+                    'choices' => $chambresArray,
+//                    'empty_data' => $offre->getChambre()->getNomchambre()
+                ])
+            ->add('categoriechambre', ChoiceType::class,
+                [
+                    'choices' => $categoriesArray,
+//                    'empty_data' => $offre->getChambre()->getNomchambre()
+                ])
+            ->add('pension', ChoiceType::class,
+                [
+                    'choices' => $pensionArray,
+                    'empty_data' => $offre->getPension()->getTypepension()
+                ])
+            ->add('tariflocal', TextType::class)
             ->add('save', SubmitType::class, ['label' => 'modifier'])
             ->getForm();
 
